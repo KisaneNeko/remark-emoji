156313290a74421d58ea6740a9b25ff05b93958d
radnowacki <radnowacki@gmail.com>
2021-09-06 11:43:34 +0200

    ignore emoji in links, only replace emojis with surrounding whitespaces

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ const DEFAULT_SETTINGS = {
     emoticon: false,
 };
 
+const IGNORED_NODES = ['link'];
+
 export default function plugin(options) {
     const settings = Object.assign({}, DEFAULT_SETTINGS, options);
     const pad = !!settings.padSpaceAfter;
     const emoticonEnable = !!settings.emoticon;
 
     function getEmojiByShortCode(match) {
+        const trimmedMatch = match ? match.trim() : match;
         // find emoji by shortcode - full match or with-out last char as it could be from text e.g. :-),
-        const iconFull = emoticon.find(e => e.emoticons.includes(match)); // full match
-        const iconPart = emoticon.find(e => e.emoticons.includes(match.slice(0, -1))); // second search pattern
-        const trimmedChar = iconPart ? match.slice(-1) : '';
+        const iconFull = emoticon.find((e) => e.emoticons.includes(trimmedMatch)); // full match
+        const iconPart = emoticon.find((e) => e.emoticons.includes(trimmedMatch.slice(0, -1))); // second search pattern
+        const trimmedChar = iconPart ? trimmedMatch.slice(-1) : '';
         const addPad = pad ? ' ' : '';
-        let icon = iconFull ? iconFull.emoji + addPad : iconPart && iconPart.emoji + addPad + trimmedChar;
-        return icon || match;
+        let icon = iconFull
+            ? iconFull.emoji + addPad
+            : iconPart && iconPart.emoji + addPad + trimmedChar;
+        return icon ? match.replace(trimmedMatch, icon) : match;
     }
 
     function getEmoji(match) {
@@ -34,10 +39,12 @@ export default function plugin(options) {
     }
 
     function transformer(tree) {
-        visit(tree, 'text', function (node) {
-            node.value = node.value.replace(RE_EMOJI, getEmoji);
-            if (emoticonEnable) {
-                node.value = node.value.replace(RE_SHORT, getEmojiByShortCode);
+        visit(tree, 'text', function (node, index, parent) {
+            if (!IGNORED_NODES.includes(parent.type)) {
+                node.value = node.value.replace(RE_EMOJI, getEmoji);
+                if (emoticonEnable) {
+                    node.value = node.value.replace(RE_SHORT, getEmojiByShortCode);
+                }
             }
         });
     }
